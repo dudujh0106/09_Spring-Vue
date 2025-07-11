e106a653f658845404c85cd0eafdda6479cd14ed
dudujh0106 <127815950+dudujh0106@users.noreply.github.com>
2025-07-11 16:07:13 +0900

    Update main.js

--- scoula/frontend/src/main.js
+++ scoula/frontend/src/main.js
@@ -10,7 +10,7 @@ import App from './App.vue';
 import router from './router';
 
 import { useKakao } from 'vue3-kakao-maps/@utils';
-const rest_api_key = '612ef664920dd4b3dfae593b6d3a3b5a'; // Javascript 키 값
+const rest_api_key = ''; // Javascript 키 값
 useKakao(rest_api_key, ['services']);
 
 const app = createApp(App);
